cfab7652af6899d31555cb267263f32fb3900836
pokeniao <1625229957@qq.com>
2025-04-02 23:20:28 +0800

    修bug

--- WPF_NET/Models/HomePageModel.cs
+++ WPF_NET/Models/HomePageModel.cs
@@ -11,4 +11,6 @@ public partial class HomePageModel : ObservableObject
 
     [ObservableProperty] private ObservableCollection<ConnectPojo> setConnectDg;
 
+    [ObservableProperty] private string currentSetName;
+
 }
