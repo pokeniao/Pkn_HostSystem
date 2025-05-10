657ae5c8cd2273f3bc41e91c75deeb979e32cbc8
pokeniao <1625229957@qq.com>
2025-05-10 16:44:50 +0800

    防止多开,且打开后最小化点击打开会回到原先大小

--- Pkn_HostSystem/App.xaml.cs
+++ Pkn_HostSystem/App.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using log4net.Config;
 using Microsoft.Extensions.DependencyInjection;
 using Pkn_HostSystem.ViewModels.Page;
 using Pkn_HostSystem.Views.Pages;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows;
 using Wpf.Ui;
 using Wpf.Ui.DependencyInjection;
@@ -13,17 +15,82 @@ namespace Pkn_HostSystem
     /// </summary>
     public partial class App : Application
     {
+
         /// <summary>
         /// 程序加载的时候
         /// </summary>
         /// <param name="e"></param>
         protected override void OnStartup(StartupEventArgs e)
         {
+            AppRunOn();
+            LogConfig();
+            CreateIoc();
+            base.OnStartup(e);
+        }
+
+        #region 唯一运行程序
+        /// <summary>
+        /// 运行唯一App
+        /// </summary>
+        private static Mutex _mutex;
+        private const string MutexName = "Pokeniao_HostSystem"; // 应用唯一标识
+        private const string MainWindowTitle = "Pkn_HostSystem"; // 用你的窗口标题替换
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int SW_RESTORE = 9;
+        private void AppRunOn()
+        {
+            bool createdNew;
+            _mutex = new Mutex(true, MutexName, out createdNew);
+
+            if (!createdNew)
+            {
+                // 找到已有窗口句柄并激活,是根据窗口标题找句柄。
+                IntPtr hWnd = FindWindow(null, MainWindowTitle);
+                if (hWnd != IntPtr.Zero)
+                {
+                    // 还原窗口
+                    ShowWindow(hWnd, SW_RESTORE);
+                    // 激活窗口
+                    SetForegroundWindow(hWnd);
+                }
+                else
+                {
+                    MessageBox.Show("程序已在运行中。", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+
+                Shutdown();
+                return;
+            }
+
+        }
+
+
+        #endregion
+
+        /// <summary>
+        /// 日志配置
+        /// </summary>
+        private void LogConfig()
+        {
             // 设置 log4net 全局变量
             log4net.GlobalContext.Properties["LOG_DIR"] = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Pkn_HostSystem", "Logs");
             // 开启 log4net 内部调试信息输出到控制台
             log4net.Util.LogLog.InternalDebugging = true;
-
+            XmlConfigurator.ConfigureAndWatch(new FileInfo("Config\\log4net.config"));
+        }
+        /// <summary>
+        /// IOC配置
+        /// </summary>
+        private void CreateIoc()
+        {
             // IOC 容器
             Ioc.Default.ConfigureServices(
                 new ServiceCollection()
@@ -45,10 +112,8 @@ namespace Pkn_HostSystem
                     .AddSingleton<SettingsPage>()
                     .AddSingleton<ModbusToolPage>()
                     .BuildServiceProvider()
-                );
-            base.OnStartup(e);
+            );
         }
-
 
         /// <summary>
         /// 程序结束的时候
@@ -58,7 +123,7 @@ namespace Pkn_HostSystem
         {
             // SettingsPageViewModel viewModel = Ioc.Default.GetRequiredService<SettingsPageViewModel>();
             // viewModel.Save();
-
+            _mutex.Dispose();
             base.OnExit(e);
         }
     }
