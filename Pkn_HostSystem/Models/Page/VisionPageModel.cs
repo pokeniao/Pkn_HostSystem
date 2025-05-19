f6ca70f97a57cf36d2433a1af1f5b2face62bb6b
pokeniao <1625229957@qq.com>
2025-05-19 21:17:27 +0800

    暂时修改的相机设置

---
+++ Pkn_HostSystem/Models/Page/VisionPageModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Pkn_HostSystem.Models.Page
+{
+    public partial class VisionPageModel:ObservableObject
+    {
+
+        [ObservableProperty] private string cameraName;
+
+        [ObservableProperty]
+        private string realTimeName = "实时";
+    }
+}
