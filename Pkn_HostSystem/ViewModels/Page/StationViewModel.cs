f9d48e683f68f76cedf9d118d9d98be0fabea608
pokeniao <1625229957@qq.com>
2025-05-26 00:11:59 +0800

    测试协同工作提交git没有Log 文件夹的提交问题

---
+++ Pkn_HostSystem/ViewModels/Page/StationViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Pkn_HostSystem.Base;
+using Pkn_HostSystem.Base.Log;
+using Pkn_HostSystem.Models.Page;
+using Wpf.Ui;
+using Wpf.Ui.Controls;
+
+namespace Pkn_HostSystem.ViewModels.Page
+{
+    public partial class StationViewModel : ObservableRecipient
+    {
+        public StationModel StationModel { get; set; } = AppJsonTool<StationModel>.Load();
+
+        public SnackbarService SnackbarService { get; set; } = new();
+
+        public LogBase<StationViewModel> Log { get; set; }
+        public StationViewModel()
+        {
+            if (StationModel == null)
+            {
+                StationModel = new StationModel();
+            }
+            else
+            {
+
+            }
+            Log = new LogBase<StationViewModel>(SnackbarService);
+        }
+        #region 弹窗SnackbarService
+        public void setSnackbarPresenter(SnackbarPresenter snackbarPresenter)
+        {
+            SnackbarService.SetSnackbarPresenter(snackbarPresenter);
+        }
+        #endregion
+
+        [RelayCommand]
+        public void Save()
+        {
+            AppJsonTool<StationModel>.Save(StationModel);
+        }
+    }
+}
---
+++ Pkn_HostSystem/Views/Pages/StationPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Pkn_HostSystem.ViewModels.Page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pkn_HostSystem.Views.Pages
+{
+    /// <summary>
+    /// StationPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class StationPage : Page
+    {
+        public StationViewModel ViewModel { get; set; }
+        public StationPage()
+        {
+            InitializeComponent();
+            ViewModel = Ioc.Default.GetRequiredService<StationViewModel>();
+            DataContext = ViewModel;
+
+        }
+    }
+}
