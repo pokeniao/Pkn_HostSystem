df34ed20f0433a0e5820431bef1c05f1b4d42422
pokeniao <1625229957@qq.com>
2025-06-16 16:57:33 +0800

    Tcp客户端服务器工具

--- Pkn_HostSystem/Models/Page/TcpToolModel.cs
+++ Pkn_HostSystem/Models/Page/TcpToolModel.cs
@@ -10,48 +10,58 @@ namespace Pkn_HostSystem.Models.Page
         [ObservableProperty] private string connectServerIp;
         //连接服务器的端口
         [ObservableProperty] private string connectServerPost;
-        //客户端接收消息
-        [ObservableProperty] private string clientAcceptMessage;
-        //客户端发送消息
-        [ObservableProperty] private string clientSendMessage;
         //服务器端口
         [ObservableProperty] private string serverPost;
-        //服务器接收消息
-        [ObservableProperty] private string serverAcceptMessage;
-        //服务器发送消息
-        [ObservableProperty] private string serverSendMessage;
         //服务器是否监听
         [ObservableProperty] private bool isLearn;
 
-        [ObservableProperty] private ObservableCollection<string> serverConnectClient = new ();
+       
         //选择客户端
         [ObservableProperty] private ObservableCollection<TcpNet> selectClient = new ();
         //选择服务器
         [ObservableProperty] private ObservableCollection<TcpNet> selectServer = new ();
+        //选中的客户端
+        [ObservableProperty] private TcpNet selectClientTcpNet;
+        //选中的服务器
+        [ObservableProperty] private TcpNet selectServerTcpNet;
+
+
     }
 
-    public class TcpNet
+    public partial class TcpNet:ObservableObject
     {
         public string Ip { get; set; }
 
         public string Post { get; set; }
 
-
+        private string ipAndPost;
         public string IpAndPost
         {
             get
             {
                 return Ip + ":" + Post;
             }
-
-            set => IpAndPost = value;
-
+            set
+            {
+                ipAndPost = value;
+                string[] strings = value.Split(':');
+                Ip = strings[3].Remove(strings[3].Length - 1);
+                Post = strings[4];
+            }
         }
+        /// <summary>
+        /// 接受消息事件
+        /// </summary>
+        public Action<string, string> OnMessageReceived;
 
-        public string AcceptMessage { get; set; }
 
-        public string SendMessage { get; set; }
 
+        [ObservableProperty] private string acceptMessage;
+
+        [ObservableProperty] private string sendMessage;
+
+        //以连接的客户端
+        [ObservableProperty] private ObservableCollection<string> serverConnectClient = new();
         public bool isLearn { get; set; }
 
         public TcpTool  TcpTool { get; set; }
