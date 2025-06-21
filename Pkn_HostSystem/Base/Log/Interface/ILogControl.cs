db6e2ae9bde26fdef9cd7debdcc2e575f5a0ebb1
pokeniao <1625229957@qq.com>
2025-06-21 10:00:16 +0800

    补提交,git漏提交的类

---
+++ Pkn_HostSystem/Base/Log/Interface/ILogControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+
+namespace Pkn_HostSystem.Base.Log.Interface
+{
+    public interface ILogControl
+    {
+         ObservableCollection<string> list { get; set; }
+
+         FlowDocument flowDocument { get; set; }
+
+         RichTextBox richTextBox { get; set; }
+    }
+}
