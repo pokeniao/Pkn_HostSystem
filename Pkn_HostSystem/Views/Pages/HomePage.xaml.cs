c41e1bb34bd88d266a1c407459fc166466a4c6ce
pokeniao <1625229957@qq.com>
2025-08-20 22:10:12 +0800

    修改字段

--- Pkn_HostSystem/Views/Pages/HomePage.xaml.cs
+++ Pkn_HostSystem/Views/Pages/HomePage.xaml.cs
@@ -67,7 +67,7 @@ namespace Pkn_HostSystem.Views.Pages
         }
 
         /// <summary>
-        /// 123工单连接卡片的展开
+        ///  工单连接卡片的展开
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -77,7 +77,7 @@ namespace Pkn_HostSystem.Views.Pages
         }
 
         /// <summary>
-        /// 123工单连接卡片的收起
+        ///  工单连接卡片的收起
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -271,7 +271,7 @@ namespace Pkn_HostSystem.Views.Pages
 
         #endregion
 
-        #region 123工单Combobox
+        #region  工单Combobox
 
         /// <summary>
         /// 下拉Combobox
